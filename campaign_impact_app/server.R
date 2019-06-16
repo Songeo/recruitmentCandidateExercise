38a54b7d405d541dd5cbf426c45d179508df795b
Songeo <sonia.gmcl@gmail.com>
2019-06-15 21:44:48 -0500

    ui y server

--- campaign_impact_app/server.R
+++ campaign_impact_app/server.R
@@ -9,18 +9,126 @@
 
 library(shiny)
 
+source("app.R")
+
+
 # Define server logic required to draw a histogram
 shinyServer(function(input, output) {
 
-    output$distPlot <- renderPlot({
-
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white')
-
+    # Reactive data dependent of rf input ----
+    
+    TblStock <- reactive({
+        fct_rf <- (input$rf_slider)
+        tbl_adstock <- 
+            tbl_data %>% 
+            mutate_at('media_campaign', factor) %>% 
+            arrange(date_week_fmt) %>% 
+            group_by(media_campaign) %>% 
+            mutate(adstock = fun_adstock_calc(media_spend_usd, fct_rf)) %>% 
+            ungroup() %>% 
+            mutate(lag = lag(search_volume)) %>% 
+            na.omit
     })
+    
+    ResMod1 <- reactive({
+        mod_list <- fun_model_lm(TblStock())
+        mod_list
+    })
+    
+    ResMod2 <- reactive({
+        mod_list <- fun_model_bayes(TblStock())
+        mod_list
+    })
+    
+    
+    # App ouput (results and plots) ----
+    output$gg_eda <- renderPlot({
+
+        # search volume
+        gg.1 <- 
+            tbl_data %>% 
+            ggplot(aes(x = date_week_fmt, 
+                       y = search_volume)) + 
+            geom_line(color = 'gray80') + 
+            geom_point(aes(color = factor(media_campaign))) + 
+            ylab('\nSearch Volume') + 
+            xlab('Date (Week)') + 
+            guides(color = guide_legend("Media\nCampaign")) + 
+            ggtitle("Time Serie: Search Volume", 
+                    "Searches realized in time by campaign")
+        
+        # media spend
+        gg.2 <- 
+            tbl_data %>% 
+            ggplot(aes(x = date_week_fmt, 
+                       y = media_spend_usd)) + 
+            geom_line(color = 'gray80') + 
+            geom_point(aes(color = factor(media_campaign))) + 
+            ylab('\nMedia Spend (USD)') + 
+            xlab('Date (Week)') + 
+            guides(color = guide_legend("Media\nCampaign")) + 
+            ggtitle("Time Serie: Media Spend", 
+                    "Advertising spend by campaign")
+        
+        
+        # adstock spend
+        gg.3 <- 
+            TblStock() %>% 
+            ggplot(aes(x = date_week_fmt, 
+                       y = adstock)) + 
+            geom_line(aes(color = factor(media_campaign)), size = 1 ) + 
+            geom_line(aes(y = media_spend_usd), color = 'gray80' ) +
+            ylab('Adstock\nMedia Spend (USD)') + 
+            xlab('Date (Week)') + 
+            guides(color = guide_legend("Media\nCampaign")) + 
+            ggtitle("Time Serie: Media Spend Adstock", 
+                    paste0("Retention factor: ", round(100*(input$rf_slider)), '%'))
+        
+        
+        grid.arrange(gg.1, gg.2, gg.3, ncol = 1)
+    })
+    
+   
+    # Results approach 1 ----
+    output$gg_mod1_fit <- renderPlot({
+        mod_list <- ResMod1()
+        gg.1 <- fun_gg_chart_fit(mod_list)
+        gg.1
+    })
+    
+    output$tab_mod1_camp <- renderDataTable({
+        mod_list <- ResMod1()
+        
+        # campaign efficiencies
+        td <- fun_tab_camp_eff(mod_list)
+        
+        DT::datatable(td, 
+                      rownames = FALSE, 
+                      options = list(searching = FALSE, paging = FALSE))
+    })
+    
+    # Results approach 2 ----
+    output$gg_mod2_fit <- renderPlot({
+        mod_list <- ResMod2()
+        gg.1 <- fun_gg_chart_fit(mod_list)
+        gg.1
+    })
+    
+    output$tab_mod2_camp <- renderDataTable({
+        mod_list <- ResMod2()
+        
+        # campaign efficiencies
+        td <- fun_tab_camp_eff(mod_list)
+        
+        DT::datatable(td, 
+                      rownames = FALSE, 
+                      options = list(searching = FALSE, paging = FALSE))
+    })
+    
+    
+    
+    
 
 })
+
+        gg.3
--- campaign_impact_app/ui.R
+++ campaign_impact_app/ui.R
@@ -7,27 +7,84 @@
 #    http://shiny.rstudio.com/
 #
 
+
 library(shiny)
 
 # Define UI for application that draws a histogram
 shinyUI(fluidPage(
-
+    
+    withMathJax(),
+    
     # Application title
-    titlePanel("Old Faithful Geyser Data"),
-
-    # Sidebar with a slider input for number of bins
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-            plotOutput("distPlot")
-        )
-    )
-))
+    titlePanel("Recruitment Candidate Exercise"),
+    br(),
+    br(),
+    
+    # Sidebar
+    sidebarPanel(width = 4,
+        h4('Input panel'),
+        helpText("Select a retention factor for the adstock function"),
+        sliderInput("rf_slider", "Retention Factor:", 
+                    min = 0, max = 1, value = .8, step = .1)
+        # actionButton("action", "Run Model", class = "btn-primary")
+        ), # sidebar Panel
+    
+    # Main panel with tabset
+    mainPanel(
+        tabsetPanel(
+            # EDA
+            tabPanel("EDA", 
+                     h3("Exploratory analysis"),
+                     "Quick plots of observed data and adstock transformation", 
+                     br(),
+                     br(),
+                     plotOutput('gg_eda', width = "650px", height = "650px")),
+            
+            # Modelo A
+            tabPanel("Approach A", 
+                     h3('Frequentist Approach'),
+                     br(),
+                     helpText(paste0('$$Search\\;Volume_{t} = \\beta_{0} + ',
+                                     '\\beta_1 * Adstock_{t} + ',
+                                     '\\sum_{i=1}^3 \\beta_{t,2,i}*campaign_i$$')),
+                     wellPanel(h4('Fitted values'),
+                               plotOutput('gg_mod1_fit', width = "700px", height = "300px")),
+                     br(),
+                     wellPanel(h4('Campaign efficiencies'),
+                               dataTableOutput('tab_mod1_camp', width = "400px")),
+                     br()
+                     ),
+            
+            # Modelo B
+            tabPanel("Approach B", 
+                     h3('Bayesian Autoregressive Approach'),
+                     br(),
+                     helpText(paste0('$$Search\\;Volume_{t} = \\beta_0 + ',
+                                     'Search\\;Volume_{t-1} + \\beta_1 * Adstock{t} + ',
+                                     '\\sum_{i=1}^3 \\beta_{t,2,i }*campaign_i$$')),
+                     wellPanel(h4('Fitted values'),
+                               plotOutput('gg_mod2_fit', width = "700px", height = "300px")),
+                     br(),
+                     wellPanel(h4('Campaign efficiencies'),
+                               dataTableOutput('tab_mod2_camp', width = "400px")),
+                     br()
+                     ),
+            
+            # Referencias
+            tabPanel("Documentation",
+                     icon =  icon("info"),
+                     includeMarkdown("documentation.Rmd"))
+            
+        ) # tabsetPanel
+    ), # mainPanel
+    
+    
+    hr(),
+    HTML('<p style="text-align:center;color:gray">
+       <b>Author: S. Mendizábal</b>
+       <br>
+       <b>Date: 2019-06-15</b>
+       </p>')
+    
+) # fluid page
+) # shinyui
